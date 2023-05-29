205941250ecc6f1c6ada084c1c63f4d9a8824b5f
anwjt <78186528+anwjt@users.noreply.github.com>
2023-05-29 22:16:45 +0700

    using js

--- resume/script.js
+++ resume/script.js
@@ -1,9 +1,12 @@
-// Smooth scrolling
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-  anchor.addEventListener('click', function(e) {
-    e.preventDefault();
-    document.querySelector(this.getAttribute('href')).scrollIntoView({
-      behavior: 'smooth'
-    });
+document.addEventListener('DOMContentLoaded', function() {
+  // Typed.js initialization
+  var typed = new Typed('#header', {
+    strings: ['Hi', 'Hello World', 'Welcome'],
+    typeSpeed: 50, // Typing speed in milliseconds
+    backSpeed: 30, // Backspacing speed in milliseconds
+    loop: true, // Enable looping
+    loopCount: Infinity, // Number of loops (use Infinity for infinite loop)
+    startDelay: 500, // Delay before typing starts in milliseconds
+    backDelay: 1500 // Delay before backspacing starts in milliseconds
   });
 });
