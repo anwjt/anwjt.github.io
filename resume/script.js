612213cb3444011ab18ebc9674f510be6a60b119
anwjt <78186528+anwjt@users.noreply.github.com>
2023-05-29 22:19:56 +0700

    uuuuuu

--- resume/script.js
+++ resume/script.js
@@ -6,7 +6,5 @@ document.addEventListener('DOMContentLoaded', function() {
     backSpeed: 30, // Backspacing speed in milliseconds
     loop: true, // Enable looping
     loopCount: Infinity, // Number of loops (use Infinity for infinite loop)
-    startDelay: 500, // Delay before typing starts in milliseconds
-    backDelay: 1500 // Delay before backspacing starts in milliseconds
   });
 });
