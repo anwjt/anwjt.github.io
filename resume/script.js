2c008ff8c0cdaeedbfc0eb2f60f1f84a19aeba77
anwjt <78186528+anwjt@users.noreply.github.com>
2023-06-01 17:09:39 +0700

    Update script.js
    update text animation

--- resume/script.js
+++ resume/script.js
@@ -1,10 +1,45 @@
-document.addEventListener('DOMContentLoaded', function() {
-  // Typed.js initialization
-  var typed = new Typed('#header', {
-    strings: ['Hi', 'Hello World', 'Welcome'],
-    typeSpeed: 50, // Typing speed in milliseconds
-    backSpeed: 30, // Backspacing speed in milliseconds
-    loop: true, // Enable looping
-    loopCount: Infinity, // Number of loops (use Infinity for infinite loop)
+const textElement = document.getElementById('typed-text');
+const texts = ['CameraMan', 'GraphicDesign', 'VideoEditor'];
+let index = 0;
+let charIndex = 0;
+let isDeleting = false;
+
+function type() {
+  const currentText = texts[index];
+  if (isDeleting) {
+    textElement.textContent = currentText.substring(0, charIndex - 1);
+    charIndex--;
+  } else {
+    textElement.textContent = currentText.substring(0, charIndex + 1);
+    charIndex++;
+  }
+
+  if (!isDeleting && charIndex === currentText.length) {
+    isDeleting = true;
+    setTimeout(type, 1500);
+  } else if (isDeleting && charIndex === 0) {
+    isDeleting = false;
+    index++;
+    if (index === texts.length) {
+      index = 0;
+    }
+    setTimeout(type, 500);
+  } else {
+    setTimeout(type, 100);
+  }
+}
+
+type();
+
+document.querySelectorAll('.nav-link').forEach(link => {
+  link.addEventListener('click', (event) => {
+    event.preventDefault();
+    const targetId = link.getAttribute('href').substring(1);
+    const targetSection = document.getElementById(targetId);
+    targetSection.scrollIntoView({ behavior: 'smooth' });
   });
+});
+window.addEventListener('scroll', function() {
+  var navbar = document.querySelector('.navbar');
+  navbar.classList.toggle('scrolled', window.scrollY > 0);
 });
