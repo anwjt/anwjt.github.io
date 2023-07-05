b56f2df78f371700e5fdb69e4d705c441f1e85f3
anwjt <78186528+anwjt@users.noreply.github.com>
2023-07-05 14:18:57 +0700

    update js

--- portfolio/script.js
+++ portfolio/script.js
@@ -1,93 +1,30 @@
 document.addEventListener("DOMContentLoaded", function() {
-    var navLinks = document.querySelectorAll("nav a");
-  
-    for (var i = 0; i < navLinks.length; i++) {
-      navLinks[i].addEventListener("click", smoothScroll);
-    }
-  
-    function smoothScroll(event) {
-      event.preventDefault();
-      var targetId = this.getAttribute("href");
-      var targetPosition = document.querySelector(targetId).offsetTop;
-      window.scrollTo({
-        top: targetPosition,
-        behavior: "smooth"
-      });
-    }
-  });
-  document.addEventListener("DOMContentLoaded", function() {
-    // Function to check if an element is in the viewport
-    function isElementInViewport(element) {
-      var rect = element.getBoundingClientRect();
-      return (
-        rect.top >= 0 &&
-        rect.left >= 0 &&
-        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-      );
-    }
-  
     // Function to handle the scroll event
-    function handleScroll() {
-      var fadeElements = document.querySelectorAll('.fade-in');
-    
-      for (var i = 0; i < fadeElements.length; i++) {
-        if (isElementInViewport(fadeElements[i])) {
-          fadeElements[i].classList.add('show');
+    function handleScroll(event) {
+      var sections = document.querySelectorAll('section');
+      var currentScrollPos = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
+  
+      for (var i = 0; i < sections.length; i++) {
+        var section = sections[i];
+        var sectionHeight = section.offsetHeight;
+        var sectionTop = section.offsetTop;
+  
+        if (currentScrollPos >= sectionTop && currentScrollPos < sectionTop + sectionHeight) {
+          window.removeEventListener('scroll', handleScroll); // Disable scroll event temporarily
+  
+          // Scroll to the next section with smooth behavior
+          var nextSectionIndex = (i + 1 < sections.length) ? i + 1 : sections.length - 1;
+          sections[nextSectionIndex].scrollIntoView({ behavior: 'smooth' });
+  
+          // Re-enable scroll event after a short delay
+          setTimeout(function() {
+            window.addEventListener('scroll', handleScroll);
+          }, 1000); // Adjust the delay duration as needed
         }
       }
     }
   
-    // Initial check on page load
-    handleScroll();
-  
     // Add scroll event listener
     window.addEventListener('scroll', handleScroll);
-  });
-  document.addEventListener("DOMContentLoaded", function() {
-  // Function to handle smooth scrolling
-  function scrollToSection(event) {
-    event.preventDefault();
-    var targetId = this.getAttribute("href");
-    var targetElement = document.querySelector(targetId);
-    targetElement.scrollIntoView({
-      behavior: "smooth"
-    });
-  }
-
-  // Attach smooth scroll event to navigation links
-  var navLinks = document.querySelectorAll("nav a");
-  for (var i = 0; i < navLinks.length; i++) {
-    navLinks[i].addEventListener("click", scrollToSection);
-  }
-
-  // Attach smooth scroll event to arrow button (if available)
-  var arrowButton = document.querySelector("#arrow-button");
-  if (arrowButton) {
-    arrowButton.addEventListener("click", scrollToSection);
-  }
-});
-document.addEventListener("DOMContentLoaded", function() {
-    // Function to handle smooth scrolling
-    function scrollToSection(event) {
-      event.preventDefault();
-      var targetId = this.getAttribute("href");
-      var targetElement = document.querySelector(targetId);
-      targetElement.scrollIntoView({
-        behavior: "smooth"
-      });
-    }
-  
-    // Attach smooth scroll event to navigation links
-    var navLinks = document.querySelectorAll("nav a");
-    for (var i = 0; i < navLinks.length; i++) {
-      navLinks[i].addEventListener("click", scrollToSection);
-    }
-  
-    // Attach smooth scroll event to arrow button (if available)
-    var arrowButton = document.querySelector("#arrow-button");
-    if (arrowButton) {
-      arrowButton.addEventListener("click", scrollToSection);
-    }
   });
   
