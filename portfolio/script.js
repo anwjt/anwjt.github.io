cd17e81ab3f249c08b15f8eb034480ec043f1198
anwjt <78186528+anwjt@users.noreply.github.com>
2023-07-05 14:07:09 +0700

    update hide mouse scrolling

--- portfolio/script.js
+++ portfolio/script.js
@@ -44,4 +44,50 @@ document.addEventListener("DOMContentLoaded", function() {
     // Add scroll event listener
     window.addEventListener('scroll', handleScroll);
   });
+  document.addEventListener("DOMContentLoaded", function() {
+  // Function to handle smooth scrolling
+  function scrollToSection(event) {
+    event.preventDefault();
+    var targetId = this.getAttribute("href");
+    var targetElement = document.querySelector(targetId);
+    targetElement.scrollIntoView({
+      behavior: "smooth"
+    });
+  }
+
+  // Attach smooth scroll event to navigation links
+  var navLinks = document.querySelectorAll("nav a");
+  for (var i = 0; i < navLinks.length; i++) {
+    navLinks[i].addEventListener("click", scrollToSection);
+  }
+
+  // Attach smooth scroll event to arrow button (if available)
+  var arrowButton = document.querySelector("#arrow-button");
+  if (arrowButton) {
+    arrowButton.addEventListener("click", scrollToSection);
+  }
+});
+document.addEventListener("DOMContentLoaded", function() {
+    // Function to handle smooth scrolling
+    function scrollToSection(event) {
+      event.preventDefault();
+      var targetId = this.getAttribute("href");
+      var targetElement = document.querySelector(targetId);
+      targetElement.scrollIntoView({
+        behavior: "smooth"
+      });
+    }
+  
+    // Attach smooth scroll event to navigation links
+    var navLinks = document.querySelectorAll("nav a");
+    for (var i = 0; i < navLinks.length; i++) {
+      navLinks[i].addEventListener("click", scrollToSection);
+    }
+  
+    // Attach smooth scroll event to arrow button (if available)
+    var arrowButton = document.querySelector("#arrow-button");
+    if (arrowButton) {
+      arrowButton.addEventListener("click", scrollToSection);
+    }
+  });
   
