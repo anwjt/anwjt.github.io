3f2a1a561e13a92f5ca6638fed808ae97101549f
anwjt <78186528+anwjt@users.noreply.github.com>
2023-12-23 16:52:40 +0700

    Add files via upload

---
+++ resume/app.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function () {
+    var typed = new Typed('.typed-text', {
+        strings: ["teseiei", "test nsjs", "testjdfvgsdgv"], // Updated strings
+        typeSpeed: 30,
+        backSpeed: 20,
+        loop: true
+    });
+});
